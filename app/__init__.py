d9b4c50ff0d863b6f4aeaffa5cfd92f552444d0b
picardcharlie <picardcharlie@gmail.com>
2022-04-28 17:22:20 -0500

    started setting up security stuff

--- app/__init__.py
+++ app/__init__.py
@@ -24,4 +24,7 @@ def create_app(config_name = "default"):
     from .main import main
     app.register_blueprint(main)
 
+    from .auth import auth
+    app.register_blueprint(auth)
+
     return app
